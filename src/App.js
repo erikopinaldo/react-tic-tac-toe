21e5d43429f6534b767e423ecae6b314aaf2dffb
Erik Opinaldo <erikopinaldo@gmail.com>
2023-02-03 15:58:17 -0500

    declare value and setValue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 
-function Square({ value }) {
+function Square() {
+  const [value, setValue] = useState(null)
+
   function handleClick() {
     console.log('clicked!');
   }
