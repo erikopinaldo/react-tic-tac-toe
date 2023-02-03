df78fb10d2011ac7d13e436bb0e675dec38efe70
Erik Opinaldo <erikopinaldo@gmail.com>
2023-02-03 16:14:46 -0500

    add spaces before closing tag for Square components

--- src/App.js
+++ src/App.js
@@ -15,18 +15,18 @@ export default function Board() {
     <>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={handleClick} />
-        <Square value={squares[1]}/>
-        <Square value={squares[2]}/>
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]}/>
-        <Square value={squares[4]}/>
-        <Square value={squares[5]}/>
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]}/>
-        <Square value={squares[7]}/>
-        <Square value={squares[8]}/>
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   )
