d14ef583517e7c510adab5732146daa9ea65f953
Erik Opinaldo <erikopinaldo@gmail.com>
2023-02-03 16:23:28 -0500

    add props as Square() arguments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-function Square() {    
+function Square({ value, onSquareClick }) {    
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
