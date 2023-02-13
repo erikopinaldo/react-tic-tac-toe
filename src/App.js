93684df48ed05a6d318fc61cdd7d9094ed22d224
Erik Opinaldo <erikopinaldo@gmail.com>
2023-02-13 18:50:35 -0500

    map history to array of buttons representing each history state

--- src/App.js
+++ src/App.js
@@ -69,6 +69,23 @@ export default function Game() {
   function jumpTo(nextMove) {
     // TODO
   }
+
+  const moves = history.map((squares, move) => {
+    let description;
+
+    if (move > 0) {
+      description = `Go to move #${move}`
+    }
+    else {
+      description = 'Go to game start'
+    }
+
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
   
   return (
     <div className='game'>
